e19f0691c411671ae698a2fded24a4a29f6616d1
maksym <marcinn580@gmail.com>
2021-03-10 20:05:47 +0100

    I10 Move utils to src directory
    src/notebook path was bad for the utils directory. Additionally, get_image_data function was moved from DFT to utils.db_helper

--- src/utils/db_helper.py
+++ src/utils/db_helper.py
@@ -22,3 +22,14 @@ def copy_files_and_resize(source_dir: str, result_dir: str, size=(256,256)):
         resized_image = image.resize(SIZE)
         new_image_path = os.path.join(RESULT_PATH, os.path.basename(image_path))
         resized_image.save(new_image_path)
+        
+def get_image_data(src_path: str, random_shuffle=True)-> t.Generator[np.ndarray, None, None]:
+    images_paths = get_files_paths_recursive(src_path)
+    if random_shuffle:
+        random.shuffle(images_paths)
+    print(f"len: {len(images_paths)}")
+    for image_path in images_paths[:200]:
+        tmp_image = Image.open(image_path)
+        np_image = np.array(tmp_image).astype(np.float32) / 255
+        tmp_image.close()
+        yield np_image
